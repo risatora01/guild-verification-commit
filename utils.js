ea4157120a1c994fdb4c43a43499fbafdcba9f47
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:21:00 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:20:53.455Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:20:58.734Z
+Commit: Patch API endpoint error handling
