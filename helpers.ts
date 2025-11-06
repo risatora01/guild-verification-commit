986c07a2e75c02b35f2427420cd0c15846466ad9
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:29:07 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:29:04.776Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T02:29:05.993Z
+Commit: Patch security vulnerability in authentication
