cfcee640f9a3dc3853db321d3cfeefe55a8b56d0
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:26:06 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:21:43.697Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T02:26:05.515Z
+Commit: Patch API endpoint error handling
