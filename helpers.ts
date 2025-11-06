a39d03e28110f1510b3577fe4886244fd832c176
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:18:52 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:18:42.404Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T02:18:51.300Z
+Commit: Correct typo in configuration file
