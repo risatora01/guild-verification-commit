b3d7ec87f2d8478f3311a04f33204a9e07b92976
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:27:35 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:27:32.899Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T02:27:34.397Z
+Commit: Fix memory leak in event handlers
