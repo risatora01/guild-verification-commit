6efd105040b0c9ce8f09b018a738178d1701356c
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:27:45 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:26:45.695Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T02:27:44.717Z
+Commit: Resolve null pointer exception in user service
