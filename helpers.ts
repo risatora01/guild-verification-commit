bd0433e11d511437af9aaee6873d62c56b83274a
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:36:58 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:36:55.758Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:36:56.975Z
+Commit: Patch security vulnerability in authentication
