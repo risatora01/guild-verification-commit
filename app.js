3a2f68ba7ffe042791d37e1eefc776dad2f9d1b0
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:21:06 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:20:36.433Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T02:21:05.504Z
+Commit: Fix broken links in documentation
