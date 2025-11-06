37533c1fadb85cabdb91a308d46434d299eb6de8
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:18:43 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:04:40.020Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T02:18:42.404Z
+Commit: Fix broken links in documentation
