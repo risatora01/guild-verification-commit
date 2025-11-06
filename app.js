10d7f1879e62c33f65bef810e6ac73cf195c4679
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:21:37 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:21:22.857Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T02:21:36.257Z
+Commit: Add multi-language support
