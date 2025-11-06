c3a6677c16d9ffc6f31abf8998639c959c5e52b5
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:20:28 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:20:12.417Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:20:27.059Z
+Commit: Fix broken links in documentation
