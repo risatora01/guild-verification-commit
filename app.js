22d0b16f70a2ed0c4bab524ad8ae048ae2130ecc
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:36:59 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:36:47.740Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T02:36:58.141Z
+Commit: Fix broken links in documentation
