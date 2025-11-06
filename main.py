a28b0ff84f7ca2e6dbce47e74140806fe8d23825
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:21:44 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:21:21.537Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T02:21:43.697Z
+Commit: Fix CSS styling bug on mobile devices
