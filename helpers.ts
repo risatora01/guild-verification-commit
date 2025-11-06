0bedee843a4422460b7640db3018e4d854b9f641
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:28:57 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:28:39.061Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T02:28:56.634Z
+Commit: Resolve timezone issues in date handling
