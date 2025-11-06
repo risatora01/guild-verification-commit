ee3a2d6a327e8f05295e88322b2a7e8eae9c078a
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:26:37 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:26:06.795Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T02:26:36.437Z
+Commit: Fix broken links in documentation
