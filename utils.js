108b05de2ab3d9eb88d3612063fbded8f4300f59
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:20:05 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:20:03.118Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T02:20:04.321Z
+Commit: Resolve timezone issues in date handling
