2b2cddfad188431c3d3fab8c80ff475e518489dc
mantore <113220845+risatora01@users.noreply.github.com>
2025-11-06 05:36:44 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:29:20.324Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T02:36:43.765Z
+Commit: Fix CSS styling bug on mobile devices
